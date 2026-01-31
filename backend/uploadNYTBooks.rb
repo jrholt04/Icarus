6c1f12450e5ea23373486147e7bc07479db7092f
jrholt04 <jrholt04@gmail.com>
2026-01-31 10:04:18 -0500

    updated nyt logic

--- backend/uploadNYTBooks.rb
+++ backend/uploadNYTBooks.rb
@@ -11,8 +11,6 @@
 $stdout.sync = true
 $stderr.reopen $stdout
 
-puts "Content-type: text/html\r\n\r\n" 
-
 require 'cgi'
 require 'mysql2'
 require 'stringio'
@@ -21,53 +19,123 @@ require 'json'
 
 icarusDB = Mysql2::Client.new(:host => '10.20.3.4', :username => 'Icarus', :password => 'B00kz!', :database => 'ss_icarus_db')
 
-uri = URI("https://api.nytimes.com/svc/books/v3/lists/current/combined-print-and-e-book-nonfiction.json?api-key=klviqNxHeAn1sJLagvrTmACJIaYZ6aPRLv6hMCABttZcAcuF")
-res = Net::HTTP.get_response(uri)
-raise "HTTP #{res.code}" unless res.is_a?(Net::HTTPSuccess)
-data = JSON.parse(res.body)
-nonFicBooks = data.dig("results", "books")
+def populateNYTFiction(db)
+    uri = URI("https://api.nytimes.com/svc/books/v3/lists/current/combined-print-and-e-book-fiction.json?api-key=klviqNxHeAn1sJLagvrTmACJIaYZ6aPRLv6hMCABttZcAcuF")
+    res = Net::HTTP.get_response(uri)
+    raise "HTTP #{res.code}" unless res.is_a?(Net::HTTPSuccess)
+    data = JSON.parse(res.body)
+    ficBooks = data.dig("results", "books")
 
-nonFicBooks.each() do |book|
-    title = book["title"].gsub("'", "\\\\'")
-    author = book["author"]
-    isbn = book["primary_isbn13"]
-    coverImage = book["book_image"]
-    description = book["description"]
-    langCode = "english"
+    titles = "("
+    ficBooks.each do |b|
+    titles += "'#{b['title'].gsub("'", "''")}',"
+    end
+    titles.chomp!(',')
+    titles += ")"
 
-    icarusDB.query("INSERT INTO Books (title, author, lang_code, isbn, cover_img, description) VALUES('" + title + "', '" + author + "', '" + langCode + "', '" + isbn.to_s() + "', '" + coverImage + "', '" + description + "');")
+    books = db.query("SELECT * FROM Books WHERE UPPER(title) IN #{titles};")
+
+    for book in books do
+        db.query("INSERT INTO NewYorkBS (book_id, category) VALUES (#{book['book_id']}, 'Fiction');")
+    end
+end 
+
+def populateNYTNonFiction(db)
+    uri = URI("https://api.nytimes.com/svc/books/v3/lists/current/combined-print-and-e-book-nonfiction.json?api-key=klviqNxHeAn1sJLagvrTmACJIaYZ6aPRLv6hMCABttZcAcuF")
+    res = Net::HTTP.get_response(uri)
+    raise "HTTP #{res.code}" unless res.is_a?(Net::HTTPSuccess)
+    data = JSON.parse(res.body)
+    nonFicBooks = data.dig("results", "books")
+
+    titles = "("
+    nonFicBooks.each do |b|
+    titles += "'#{b['title'].gsub("'", "''")}',"
+    end
+    titles.chomp!(',')
+    titles += ")"
+
+    books = db.query("SELECT * FROM Books WHERE UPPER(title) IN #{titles};")
+
+    for book in books do
+        db.query("INSERT INTO NewYorkBS (book_id, category) VALUES (#{book['book_id']}, 'Non-Fiction');")
+    end
+end 
+
+def populateNYTSelfHelp(db)
+    uri = URI("https://api.nytimes.com/svc/books/v3/lists/current/advice-how-to-and-miscellaneous.json?api-key=klviqNxHeAn1sJLagvrTmACJIaYZ6aPRLv6hMCABttZcAcuF")
+    res = Net::HTTP.get_response(uri)
+    raise "HTTP #{res.code}" unless res.is_a?(Net::HTTPSuccess)
+    data = JSON.parse(res.body)
+    selfHelpBooks = data.dig("results", "books")
+
+    titles = "("
+    selfHelpBooks.each do |b|
+    titles += "'#{b['title'].gsub("'", "''")}',"
+    end
+    titles.chomp!(',')
+    titles += ")"
+
+    books = db.query("SELECT * FROM Books WHERE UPPER(title) IN #{titles};")
+    
+    for book in books do
+        db.query("INSERT INTO NewYorkBS (book_id, category) VALUES (#{book['book_id']}, 'Self-Help');")
+    end
 end
 
-uri = URI("https://api.nytimes.com/svc/books/v3/lists/current/combined-print-and-e-book-fiction.json?api-key=klviqNxHeAn1sJLagvrTmACJIaYZ6aPRLv6hMCABttZcAcuF")
-res = Net::HTTP.get_response(uri)
-raise "HTTP #{res.code}" unless res.is_a?(Net::HTTPSuccess)
-data = JSON.parse(res.body)
-fictionBooks = data.dig("results", "books")
+# Clear out the NewYorkBS table 
+icarusDB.query("DELETE FROM NewYorkBS;")
+populateNYTFiction(icarusDB)
+populateNYTNonFiction(icarusDB)
+populateNYTSelfHelp(icarusDB)
 
-fictionBooks.each() do |book|
-    title = book["title"].gsub("'", "\\\\'")
-    author = book["author"]
-    isbn = book["primary_isbn13"]
-    coverImage = book["book_image"]
-    description = book["description"]
-    langCode = "english"
+# uri = URI("https://api.nytimes.com/svc/books/v3/lists/current/combined-print-and-e-book-nonfiction.json?api-key=klviqNxHeAn1sJLagvrTmACJIaYZ6aPRLv6hMCABttZcAcuF")
+# res = Net::HTTP.get_response(uri)
+# raise "HTTP #{res.code}" unless res.is_a?(Net::HTTPSuccess)
+# data = JSON.parse(res.body)
+# nonFicBooks = data.dig("results", "books")
 
-    icarusDB.query("INSERT INTO Books (title, author, lang_code, isbn, cover_img, description) VALUES('" + title + "', '" + author + "', '" + langCode + "', '" + isbn.to_s() + "', '" + coverImage + "', '" + description + "');")
-end
+# nonFicBooks.each() do |book|
+#     title = book["title"].gsub("'", "\\\\'")
+#     author = book["author"]
+#     isbn = book["primary_isbn13"]
+#     coverImage = book["book_image"]
+#     description = book["description"]
+#     langCode = "english"
 
-uri = URI("https://api.nytimes.com/svc/books/v3/lists/current/advice-how-to-and-miscellaneous.json?api-key=klviqNxHeAn1sJLagvrTmACJIaYZ6aPRLv6hMCABttZcAcuF")
-res = Net::HTTP.get_response(uri)
-raise "HTTP #{res.code}" unless res.is_a?(Net::HTTPSuccess)
-data = JSON.parse(res.body)
-howToBooks = data.dig("results", "books")
+#     icarusDB.query("INSERT INTO Books (title, author, lang_code, isbn, cover_img, description) VALUES('" + title + "', '" + author + "', '" + langCode + "', '" + isbn.to_s() + "', '" + coverImage + "', '" + description + "');")
+# end
 
-howToBooks.each() do |book|
-    title = book["title"].gsub("'", "\\\\'")
-    author = book["author"]
-    isbn = book["primary_isbn13"]
-    coverImage = book["book_image"]
-    description = book["description"]
-    langCode = "english"
+# uri = URI("https://api.nytimes.com/svc/books/v3/lists/current/combined-print-and-e-book-fiction.json?api-key=klviqNxHeAn1sJLagvrTmACJIaYZ6aPRLv6hMCABttZcAcuF")
+# res = Net::HTTP.get_response(uri)
+# raise "HTTP #{res.code}" unless res.is_a?(Net::HTTPSuccess)
+# data = JSON.parse(res.body)
+# fictionBooks = data.dig("results", "books")
 
-    icarusDB.query("INSERT INTO Books (title, author, lang_code, isbn, cover_img, description) VALUES('" + title + "', '" + author + "', '" + langCode + "', '" + isbn.to_s() + "', '" + coverImage + "', '" + description + "');")
-end
+# fictionBooks.each() do |book|
+#     title = book["title"].gsub("'", "\\\\'")
+#     author = book["author"]
+#     isbn = book["primary_isbn13"]
+#     coverImage = book["book_image"]
+#     description = book["description"]
+#     langCode = "english"
+
+#     icarusDB.query("INSERT INTO Books (title, author, lang_code, isbn, cover_img, description) VALUES('" + title + "', '" + author + "', '" + langCode + "', '" + isbn.to_s() + "', '" + coverImage + "', '" + description + "');")
+# end
+
+# uri = URI("https://api.nytimes.com/svc/books/v3/lists/current/advice-how-to-and-miscellaneous.json?api-key=klviqNxHeAn1sJLagvrTmACJIaYZ6aPRLv6hMCABttZcAcuF")
+# res = Net::HTTP.get_response(uri)
+# raise "HTTP #{res.code}" unless res.is_a?(Net::HTTPSuccess)
+# data = JSON.parse(res.body)
+# howToBooks = data.dig("results", "books")
+
+# howToBooks.each() do |book|
+#     title = book["title"].gsub("'", "\\\\'")
+#     author = book["author"]
+#     isbn = book["primary_isbn13"]
+#     coverImage = book["book_image"]
+#     description = book["description"]
+#     langCode = "english"
+
+#     icarusDB.query("INSERT INTO Books (title, author, lang_code, isbn, cover_img, description) VALUES('" + title + "', '" + author + "', '" + langCode + "', '" + isbn.to_s() + "', '" + coverImage + "', '" + description + "');")
+# end
+
